659222ecfaea2db3a762abfba0051bdcce4d63a2
Marvin W <marvwood12@gmail.com>
2021-04-15 13:31:19 -0400

    win dectection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 10;
+const cells = 6;
 const width = 600;
 const height = 600;
 
@@ -127,6 +127,7 @@ horizontals.forEach((row, rowIndex) => {
       unitLength,
       5,
       {
+        label: 'wall',
         isStatic: true,
       }
     );
@@ -146,6 +147,7 @@ verticals.forEach((row, rowIndex) => {
       5,
       unitLength,
       {
+        label: 'wall',
         isStatic: true,
       }
     );
@@ -159,6 +161,7 @@ const goal = Bodies.rectangle(
   unitLength * 0.7,
   unitLength * 0.7,
   {
+    label: "goal",
     isStatic: true,
   }
 );
@@ -167,26 +170,43 @@ World.add(world, goal);
 
 //Ball
 
-const ball = Bodies.circle(
-  unitLength / 2,
-  unitLength / 2,
-  unitLength / 4,
-);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+  label: "ball",
+});
 World.add(world, ball);
 
-document.addEventListener('keydown', event => {
+document.addEventListener("keydown", (event) => {
   const { x, y } = ball.velocity;
 
-  if(event.keyCode === 87){
-    Body.setVelocity(ball, { x, y: - 5 })
+  if (event.keyCode === 87) {
+    Body.setVelocity(ball, { x, y: -5 });
   }
-  if(event.keyCode === 68){
-    Body.setVelocity(ball, { x: x + 5, y})
+  if (event.keyCode === 68) {
+    Body.setVelocity(ball, { x: x + 5, y });
   }
-  if(event.keyCode === 83){
-    Body.setVelocity(ball, { x, y: + 5 })
+  if (event.keyCode === 83) {
+    Body.setVelocity(ball, { x, y: +5 });
   }
-  if(event.keyCode === 65){
-    Body.setVelocity(ball, { x: x- 5, y})
+  if (event.keyCode === 65) {
+    Body.setVelocity(ball, { x: x - 5, y });
   }
+});
+
+// win condition
+Events.on(engine, "collisionStart", (event) => {
+  event.pairs.forEach((collision) => {
+    const labels = ["ball", "goal"];
+
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      world.gravity.y =1;
+      world.bodies.forEach(body => {
+        if(body.label === 'wall'){
+          Body.setStatic(body, false);
+        }
+      })
+    }
+  });
 });
