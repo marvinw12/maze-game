95184646f779bfb0d82ca0256b40bd3d81095b4d
Marvin W <marvwood12@gmail.com>
2021-04-15 14:02:38 -0400

    added fill colors + display sucess msg

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    wireframes: true,
+    wireframes: false,
     width,
     height,
   },
@@ -131,6 +131,9 @@ horizontals.forEach((row, rowIndex) => {
       {
         label: 'wall',
         isStatic: true,
+        render: {
+          fillStyle: 'red'
+        }
       }
     );
     World.add(world, wall);
@@ -151,6 +154,9 @@ verticals.forEach((row, rowIndex) => {
       {
         label: 'wall',
         isStatic: true,
+        render: {
+          fillStyle: 'red'
+        }
       }
     );
     World.add(world, wall);
@@ -165,6 +171,9 @@ const goal = Bodies.rectangle(
   {
     label: "goal",
     isStatic: true,
+    render: {
+      fillStyle: 'green'
+    }
   }
 );
 
@@ -175,6 +184,9 @@ World.add(world, goal);
 const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
 const ball = Bodies.circle(unitLengthX / 2, unitLengthY / 2, ballRadius, {
   label: "ball",
+  render: {
+    fillStyle: 'blue'
+  }
 });
 World.add(world, ball);
 
@@ -204,6 +216,7 @@ Events.on(engine, "collisionStart", (event) => {
       labels.includes(collision.bodyA.label) &&
       labels.includes(collision.bodyB.label)
     ) {
+      document.querySelector('.winner').classList.remove('hidden');
       world.gravity.y =1;
       world.bodies.forEach(body => {
         if(body.label === 'wall'){
